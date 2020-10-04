066e06f50f93afa29b56b74e25f75a0e871c9974
Leandro Bezerra Marinho <leandrobezerramarinho@gmail.com>
2020-10-03 23:46:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import k_means_clustering as km
 import cv2
 
-imagem=cv2.imread('4.png')
+# imagem=cv2.imread('4.png')
+imagem=cv2.imread('2.png')
 
 imagem = km.kmeans_segmentation(imagem, 6)
 
@@ -9,3 +10,4 @@ cv2.imshow('teste',km.kmeans_segmentation(imagem,3))
 cv2.waitKey(0)
 
 # cv2.imread('teste',imagem)
+# cv2.imread('teste2',imagem)
